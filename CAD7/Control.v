0e2176d4d0097ed6bfdaf222845a119a190a6dc5
Matthias Yee <matthias_yee@brown.edu>
2026-04-20 00:13:11 -0400

    Control.v "done"

--- CAD7/Control.v
+++ CAD7/Control.v
@@ -1,30 +1,44 @@
 module Control (
-    input [15:0] instr,  // Full 16-bit instruction word
-    input        psr_z,  // Zero flag from PSR
-    input        psr_n,  // Negative flag from PSR
-    input        psr_f,  // Lower than flag from PSR
+    input [15:0] instr,
 
-    // Outputs to Register File
+    // Register File
     output reg       reg_write,
-    output     [3:0] rd,         // For your WE[15..0] decoder
-    output     [3:0] ra,         // Port A (RA)
-    output     [3:0] rb,         // Port B (RB)
+    output     [3:0] rd,
+    output     [3:0] ra,
+    output     [3:0] rb,
 
-    // Outputs to ALU & Shifter
-    output reg [1:0] alu_sel,  // Map to your 2-bit SEL
-    output reg       alu_src,  // 0: Reg, 1: Immediate
-    output reg       shf_en,
-    output     [7:0] imm,
+    // ALU
+    output reg [1:0] alu_sel,    // 00: ADD, 01: AND, 10: OR, 11: XOR
+    output reg       alu_src_b,  // 0: Reg, 1: Immediate
+    output reg       alu_src_a,  // 0: Reg, 1: Imm 0 (used for MOV)
+    output reg       cin,        // 1 for sub/cmp, 0 otherwise
+
+    // Imm Gen
+    output [7:0] imm,
+    output reg extend,  // 0: zero extend, 1: sign extend
+
+    // Shift
+    output reg shift_val_src,  // 0: reg, 1: imm
+    output reg [1:0] shift_amt_src,  // 00: reg, 01: imm, 10: 8
 
     // Memory Signals
-    output reg mem_read,
     output reg mem_write,
-    output reg mem_to_reg,
+
+    output reg [1:0] data_out,  // 00: ALU, 01: Mem, 10: Shifter
+
+    output reg pc_write,  // 0: data_out to RF, 1: PC to RF
 
     // Outputs to PC
     output reg pc_br,  // For Branch
-    output reg pc_jmp  // For Jump
+    output reg pc_jmp, // For Jump
+
+    output reg [7:0] disp
 );
+
+
+  reg psr_z;
+  reg psr_n;
+  reg psr_f;
 
   wire [3:0] opcode = instr[15:12];
   wire [3:0] rdest = instr[11:8];
@@ -32,10 +46,11 @@ module Control (
   wire [3:0] rsrc = instr[3:0];
 
 
-  assign rd  = rdest;
-  assign ra  = rdest;
-  assign rb  = rsrc;
-  assign imm = instr[7:0];
+  assign rd   = rdest;
+  assign ra   = rsrc;
+  assign rb   = dest;
+  assign imm  = instr[7:0];
+  assign disp = instr[7:0];
 
   reg cond_met;
   always @(*) begin
@@ -63,88 +78,184 @@ module Control (
   always @(*) begin
     reg_write = 0;
     alu_sel = 2'b00;
-    alu_src = 0;
-    shf_en = 0;
-    mem_read = 0;
+    alu_src_b = 0;
+    alu_src_a = 0;
+    cin = 0;
+    extend = 0;
     mem_write = 0;
-    mem_to_reg = 0;
+    data_out = 2'b00;
+    pc_write = 0;
     pc_br = 0;
     pc_jmp = 0;
 
     case (opcode)
       4'b0000: begin  // Register
         reg_write = 1;
-        alu_src   = 0;
+        alu_src_b = 0;
+        alu_src_a = 0;
+        cin = 0;
+        extend = 0;
+        mem_write = 0;
+        data_out = 2'b00;
+        pc_write = 0;
+
         case (ext)
           4'b0101: alu_sel = 2'b00;  // ADD
-          4'b1001: alu_sel = 2'b00;  // SUB
+          4'b1001: begin
+            alu_sel = 2'b00;  // SUB
+            cin = 1;
+          end
           4'b1011: begin  // CMP (Subtract but no writeback)
             reg_write = 0;
-            alu_sel   = 2'b01;
+            alu_sel = 2'b00;
+            cin = 1;
           end
-          4'b0001: alu_sel = 2'b10;  // AND
-          4'b0010: alu_sel = 2'b11;  // OR
-          4'b0011: ;  // XOR
-          4'b1101: ;  // MOV
+          4'b0001: alu_sel = 2'b01;  // AND
+          4'b0010: alu_sel = 2'b10;  // OR
+          4'b0011: alu_sel = 2'b11;  // XOR
+          4'b1101: begin
+            alu_sel   = 2'b00;  // MOV
+            alu_src_a = 1;
+          end
         endcase
       end
 
       4'b0101: begin  // ADDI
         reg_write = 1;
-        alu_src   = 1;
-        alu_sel   = 2'b00;
+        alu_src_b = 1;
+        alu_src_a = 0;
+        alu_sel = 2'b00;
+        cin = 0;
+        extend = 1;
+        mem_write = 0;
+        data_out = 2'b00;
+        pc_write = 0;
       end
 
       4'b1001: begin  // SUBI
         reg_write = 1;
-        alu_src   = 1;
-        alu_sel   = 2'b01;
+        alu_src_b = 1;
+        alu_src_a = 0;
+        alu_sel = 2'b00;
+        cin = 1;
+        extend = 1;
+        mem_write = 0;
+        data_out = 2'b00;
+        pc_write = 0;
       end
 
       4'b1101: begin  // CMPI
+        reg_write = 0;
+        alu_src_b = 1;
+        alu_src_a = 0;
+        alu_sel = 2'b00;
+        cin = 1;
+        extend = 1;
+        mem_write = 0;
+        data_out = 2'b00;
+        pc_write = 0;
       end
 
       4'b0001: begin  // ANDI
-        // Zero extend imm
+        reg_write = 1;
+        alu_src_b = 1;
+        alu_src_a = 0;
+        alu_sel = 2'b01;
+        cin = 0;
+        extend = 0;
+        mem_write = 0;
+        data_out = 2'b00;
+        pc_write = 0;
       end
 
       4'b0010: begin  // ORI
-        // Zero extend imm
+        reg_write = 1;
+        alu_src_b = 1;
+        alu_src_a = 0;
+        alu_sel = 2'b10;
+        cin = 0;
+        extend = 0;
+        mem_write = 0;
+        data_out = 2'b00;
+        pc_write = 0;
       end
 
       4'b0011: begin  // XORI
-        // Zero extend imm
+        reg_write = 1;
+        alu_src_b = 1;
+        alu_src_a = 0;
+        alu_sel = 2'b11;
+        cin = 0;
+        extend = 0;
+        mem_write = 0;
+        data_out = 2'b00;
+        pc_write = 0;
       end
 
       4'b1011: begin  // MOVI
-        // Zero extend imm
+        reg_write = 1;
+        alu_src_b = 1;
+        alu_src_a = 1;
+        alu_sel = 2'b00;
+        cin = 0;
+        extend = 0;
+        mem_write = 0;
+        data_out = 2'b00;
+        pc_write = 0;
       end
 
       4'b1000: begin  // SHIFT
+        reg_write = 1;
+        mem_write = 0;
+        data_out  = 2'b10;
+        pc_write  = 0;
         case (ext)
-          4'b0100: ;  // LSH
-          4'b0000: ;  // LSHI 0 extend?
-          4'b0001: ;  // LSHI 2s comp extend
+          4'b0100: begin  // LSH
+            shift_val_src = 0;
+            shift_amt_src = 2'b00;
+          end
+          4'b0000: begin  // LSHI 0 extend?
+            extend = 0;
+            shift_val_src = 0;
+            shift_amt_src = 2'b01;
+          end
+          4'b0001: begin  // LSHI 2s comp extend
+            extend = 1;
+            shift_val_src = 0;
+            shift_amt_src = 2'b01;
+          end
         endcase
       end
 
-      4'b1111: ;  // LUI
+      4'b1111: begin  // LUI
+        reg_write = 1;
+        mem_write = 0;
+        data_out = 2'b10;
+        pc_write = 0;
+        extend = 0;
+        shift_val_src = 1;
+        shift_amt_src = 2'b11;
+      end
 
 
       4'b0100: begin
         case (ext)
           4'b0000: begin  // LOAD
-            reg_write  = 1;
-            mem_read   = 1;
-            mem_to_reg = 1;
+            reg_write = 1;
+            data_out  = 2'b01;
           end
           4'b0100: begin  // STOR
+            reg_write = 0;
             mem_write = 1;
           end
           4'b1100: begin  // JCOND
             if (cond_met) pc_jmp = 1;
           end
-          4'b1000: pc_jmp = 1;  // JAL
+          4'b1000: begin
+            pc_jmp = 1;  // JAL
+            pc_write = 1;
+            reg_write = 1;
+          end
         endcase
       end
 
