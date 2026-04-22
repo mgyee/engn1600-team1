6148e3a68136bf34ee13481924991f45bb586404
Matthias Yee <matthias_yee@brown.edu>
2026-04-22 17:46:51 -0400

    mem.v files

---
+++ CAD7/imem.v
@@ -0,0 +1,17 @@
+module imem #(
+    parameter DEPTH   = 65536,
+    parameter MEMFILE = ""
+) (
+    input  wire [15:0] addr,
+    output wire [15:0] data
+);
+  reg [15:0] mem[0:DEPTH-1];
+
+  initial begin
+    integer i;
+    for (i = 0; i < DEPTH; i = i + 1) mem[i] = 16'h0000;
+    if (MEMFILE != "") $readmemh(MEMFILE, mem);
+  end
+
+  assign data = mem[addr];
+endmodule
